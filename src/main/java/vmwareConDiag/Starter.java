4bb59cc19d6e9b20d5acfb4f65f904cf4a46e303
Ronny Trommer <ronny@opennms.org>
2013-11-22 15:41:20 +0100

    Added iterator to list all VirtualMachines and HostSystems

--- src/main/java/vmwareConDiag/Starter.java
+++ src/main/java/vmwareConDiag/Starter.java
@@ -2,7 +2,7 @@
  * *****************************************************************************
  * This file is part of OpenNMS(R).
  *
- * Copyright (C) 2012 The OpenNMS Group, Inc. OpenNMS(R) is Copyright (C) 1999-2013 The OpenNMS Group, Inc.
+ * Copyright (C) 2013 The OpenNMS Group, Inc. OpenNMS(R) is Copyright (C) 1999-2013 The OpenNMS Group, Inc.
  *
  * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
  *
@@ -20,6 +20,8 @@
  */
 package vmwareConDiag;
 
+import com.vmware.vim25.mo.InventoryNavigator;
+import com.vmware.vim25.mo.ManagedEntity;
 import com.vmware.vim25.mo.ServiceInstance;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.slf4j.Logger;
@@ -38,44 +40,138 @@ import java.util.Properties;
  */
 public class Starter {
 
+    /**
+     * Config file for vCenter host and credentials
+     */
+    private static final String CONFIG_PROPERTIES = "config.properties";
+
+    /**
+     * Host property with vCenter IP or FQDN
+     */
+    private static final String PROP_HOST = "host";
+
+    /**
+     * User property for vCenter login
+     */
+    private static final String PROP_USER = "user";
+
+    /**
+     * Password property with vCenter user password
+     */
+    private static final String PROP_PASS = "pass";
+
+    /**
+     * Constant for empty string
+     */
+    private static final String EMPTY_STRING = "";
+
+    /**
+     * vCenter query string for host systems
+     */
+    private static final String VMWARE_HOSTSYSTEM = "HostSystem";
+
+    /**
+     * vCenter query string for virtual machines
+     */
+    private static final String VMWARE_VIRTUALMACHINE = "VirtualMachine";
+
+    /**
+     * Initialize logging
+     */
     private static Logger logger = LoggerFactory.getLogger(Starter.class);
 
+    /**
+     * Main method to test if connection to a vCenter can established. It loads also a config.properties with
+     * user credentials for vCenter to establish the connection using the ViJavaConnectionTest.
+     * After established connection some "about information" from the vCenter are received and the connection
+     * will be closed.
+     *
+     * @param args - No args evaluated
+     */
     public static void main(String[] args) {
-        String host = "";
-        String user = "";
-        String pass = "";
+        // Init vCenter credentials
+        String host = EMPTY_STRING;
+        String user = EMPTY_STRING;
+        String pass = EMPTY_STRING;
 
+        // Used for read in config.properties
         Properties properties = new Properties();
 
+        // vCenter service instance used if a vCenter connection could be established
+        ServiceInstance serviceInstance;
+
+        // Load config.properties from current directory
         try {
-            properties.load(new FileInputStream("config.properties"));
-            host = properties.getProperty("host");
-            user = properties.getProperty("user");
-            pass = properties.getProperty("pass");
+            properties.load(new FileInputStream(CONFIG_PROPERTIES));
+            host = properties.getProperty(PROP_HOST);
+            user = properties.getProperty(PROP_USER);
+            pass = properties.getProperty(PROP_PASS);
         } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            logger.error("Couldn't read configuration property ['{}']. Error message: '{}'", CONFIG_PROPERTIES, e.getMessage());
+            logger.debug("Stack trace: '{}'", CONFIG_PROPERTIES, e.getMessage(), e.getStackTrace());
+
+            // No vCenter credentials --> Error exit
             System.exit(1);
         }
 
+        // Could read config.properties.
         logger.info("Using host:'{}', user:'{}', pass (SHA-256):'{}' for connection", host, user, DigestUtils.sha256Hex(pass));
-        ServiceInstance serviceInstance;
 
-        ViJavaConnectTest viJavaConnectTest = new ViJavaConnectTest(host,user,pass);
+        // Initialize connection with vCenter credentials
+        ViJavaConnectTest viJavaConnectTest = new ViJavaConnectTest(host, user, pass);
 
+        // Try to establish the connection to vCenter
         try {
             logger.info("Try to connect: '{}'", host);
+
+            // Establish connection
             serviceInstance = viJavaConnectTest.connect();
+
+            // Give some information to test if connection and credentials work
             logger.info("Connect successfull");
-            logger.info("VMware API Type:         '{}'",serviceInstance.getAboutInfo().apiType);
+            logger.info("VMware API Type:         '{}'", serviceInstance.getAboutInfo().apiType);
             logger.info("VMware API Version:      '{}' build '{}'", serviceInstance.getAboutInfo().apiVersion, serviceInstance.getAboutInfo().build);
             logger.info("VMware operating system: '{}'", serviceInstance.getAboutInfo().getOsType());
 
+            // Give some information about VMware systems
+            iterateVmwareSystems(serviceInstance, VMWARE_HOSTSYSTEM);
+            iterateVmwareSystems(serviceInstance, VMWARE_VIRTUALMACHINE);
+
         } catch (MalformedURLException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            logger.error("Malformed URL exception occurred. Error message: '{}'", e.getMessage());
+            logger.debug("Stack trace: '{}'", e.getStackTrace());
+
+            // Connection not possible --> Error exit
+            System.exit(1);
         } catch (RemoteException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            logger.error("Remote exception occurred. Error message: '{}'", e.getMessage());
+            logger.debug("Stack trace: '{}'", e.getStackTrace());
+
+            // Connection not possible --> Error exit
+            System.exit(1);
         }
 
+        // Disconnect vCenter connection
         viJavaConnectTest.disconnect();
+    }
+
+    /**
+     * Search on a vCenter for a specific system type i.e. VirtualMachine or Host System.
+     * @param serviceInstance {@link  com.vmware.vim25.mo.ServiceInstance} with established vCenter connection
+     * @param systemType Search type for managed entity, i.e. VirtualMachine or Host System as {@link java.lang.String}
+     * @throws RemoteException
+     */
+    private static void iterateVmwareSystems(ServiceInstance serviceInstance, String systemType) throws RemoteException {
+        ManagedEntity[] vmwareSystems;
+        // Search for system type on vCenter as ManagedEntity array
+        vmwareSystems = new InventoryNavigator(serviceInstance.getRootFolder()).searchManagedEntities(systemType);
+
+        logger.info("Systems for type '{}' found: '{}'", systemType, vmwareSystems.length);
+        logger.info("Show system names:");
+
+        // Display name for each virtual machine or host system
+        for (ManagedEntity entity : vmwareSystems) {
+            logger.info("-> Type: '{}' :: Name: '{}'", systemType, entity.getName());
+        }
     }
 }
--- src/main/java/vmwareConDiag/ViJavaConnectTest.java
+++ src/main/java/vmwareConDiag/ViJavaConnectTest.java
@@ -28,27 +28,20 @@
 
 package vmwareConDiag;
 
-import com.vmware.vim25.*;
-import com.vmware.vim25.mo.*;
-import com.vmware.vim25.mo.util.MorUtil;
+import com.vmware.vim25.VimPortType;
+import com.vmware.vim25.mo.ServerConnection;
+import com.vmware.vim25.mo.ServiceInstance;
 import com.vmware.vim25.ws.WSClient;
-import org.sblim.wbem.cim.*;
-import org.sblim.wbem.client.CIMClient;
-import org.sblim.wbem.client.PasswordCredential;
-import org.sblim.wbem.client.UserPrincipal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.net.ssl.*;
-import java.net.ConnectException;
-import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.rmi.RemoteException;
 import java.security.SecureRandom;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-import java.util.*;
 
 /**
  * <p>ViJavaConnectTest class.</p>
@@ -61,19 +54,27 @@ public class ViJavaConnectTest {
     /**
      * logging for VMware library VI Java
      */
-    private final Logger logger = LoggerFactory.getLogger("OpenNMS.VMware." + ViJavaConnectTest.class.getName());
+    private final Logger logger = LoggerFactory.getLogger("vmwareConDiag." + ViJavaConnectTest.class.getName());
 
-    private String m_hostname = null;
+    /**
+     * vCenter IP or FQDN
+     */
+    private String hostname = null;
 
-    private String m_username = null;
+    /**
+     * vCenter username for login
+     */
+    private String username = null;
 
-    private String m_password = null;
+    /**
+     * vCenter user password for login
+     */
+    private String password = null;
 
-    private ServiceInstance m_serviceInstance = null;
-
-    private Map<HostSystem, HostServiceTicket> m_hostServiceTickets = new HashMap<HostSystem, HostServiceTicket>();
-
-    private Map<HostSystem, String> m_hostSystemCimUrls = new HashMap<HostSystem, String>();
+    /**
+     * vCenter service instance with established connection
+     */
+    private ServiceInstance serviceInstance = null;
 
     /**
      * Constructor for creating a instance for a given server and credentials.
@@ -83,9 +84,9 @@ public class ViJavaConnectTest {
      * @param password the password
      */
     public ViJavaConnectTest(String hostname, String username, String password) {
-        this.m_hostname = hostname;
-        this.m_username = username;
-        this.m_password = password;
+        this.hostname = hostname;
+        this.username = username;
+        this.password = password;
     }
 
     /**
@@ -97,7 +98,7 @@ public class ViJavaConnectTest {
     public ServiceInstance connect() throws MalformedURLException, RemoteException {
         relax();
 
-        return new ServiceInstance(new URL("https://" + m_hostname + "/sdk"), m_username, m_password);
+        return new ServiceInstance(new URL("https://" + hostname + "/sdk"), username, password);
     }
 
     /**
@@ -107,8 +108,8 @@ public class ViJavaConnectTest {
      * @return true, if the operation was successful
      */
     public boolean setTimeout(int timeout) {
-        if (m_serviceInstance != null) {
-            ServerConnection serverConnection = m_serviceInstance.getServerConnection();
+        if (serviceInstance != null) {
+            ServerConnection serverConnection = serviceInstance.getServerConnection();
             if (serverConnection != null) {
                 VimPortType vimService = serverConnection.getVimService();
                 if (vimService != null) {
@@ -128,17 +129,17 @@ public class ViJavaConnectTest {
      * Disconnects from the server.
      */
     public void disconnect() {
-        if (m_serviceInstance == null) {
+        if (serviceInstance == null) {
             // not connected
             return;
         } else {
-            ServerConnection serverConnection = m_serviceInstance.getServerConnection();
+            ServerConnection serverConnection = serviceInstance.getServerConnection();
 
             if (serverConnection == null) {
                 // not connected
                 return;
             } else {
-                m_serviceInstance.getServerConnection().logout();
+                serviceInstance.getServerConnection().logout();
             }
         }
     }
@@ -177,287 +178,6 @@ public class ViJavaConnectTest {
                 return true;
             }
         });
-    }
-
-    /**
-     * Returns a managed entity for a given managed object Id.
-     *
-     * @param managedObjectId the managed object Id
-     * @return the managed entity
-     */
-    public ManagedEntity getManagedEntityByManagedObjectId(String managedObjectId) {
-        ManagedObjectReference managedObjectReference = new ManagedObjectReference();
-
-        managedObjectReference.setType("ManagedEntity");
-        managedObjectReference.setVal(managedObjectId);
-
-        ManagedEntity managedEntity = MorUtil.createExactManagedEntity(m_serviceInstance.getServerConnection(), managedObjectReference);
-
-        return managedEntity;
-    }
-
-    /**
-     * Returns a virtual machine by a given managed object Id.
-     *
-     * @param managedObjectId the managed object Id
-     * @return the virtual machine object
-     */
-    public VirtualMachine getVirtualMachineByManagedObjectId(String managedObjectId) {
-        ManagedObjectReference managedObjectReference = new ManagedObjectReference();
-
-        managedObjectReference.setType("VirtualMachine");
-        managedObjectReference.setVal(managedObjectId);
-
-        VirtualMachine virtualMachine = (VirtualMachine) MorUtil.createExactManagedEntity(m_serviceInstance.getServerConnection(), managedObjectReference);
-
-        return virtualMachine;
-    }
-
-    /**
-     * Returns a host system by a given managed object Id.
-     *
-     * @param managedObjectId the managed object Id
-     * @return the host system object
-     */
-    public HostSystem getHostSystemByManagedObjectId(String managedObjectId) {
-        ManagedObjectReference managedObjectReference = new ManagedObjectReference();
-
-        managedObjectReference.setType("HostSystem");
-        managedObjectReference.setVal(managedObjectId);
-
-        HostSystem hostSystem = (HostSystem) MorUtil.createExactManagedEntity(m_serviceInstance.getServerConnection(), managedObjectReference);
-
-        return hostSystem;
-    }
-
-    /**
-     * Queries a host system for Cim data.
-     *
-     * @param hostSystem       the host system to query
-     * @param cimClass         the class of Cim objects to retrieve
-     * @param primaryIpAddress the Ip address to use
-     * @return the list of Cim objects
-     * @throws RemoteException
-     * @throws org.sblim.wbem.cim.CIMException
-     *
-     */
-    public List<CIMObject> queryCimObjects(HostSystem hostSystem, String cimClass, String primaryIpAddress) throws ConnectException, RemoteException, CIMException {
-        List<CIMObject> cimObjects = new ArrayList<CIMObject>();
-
-        if (!m_hostServiceTickets.containsKey(hostSystem)) {
-            m_hostServiceTickets.put(hostSystem, hostSystem.acquireCimServicesTicket());
-        }
-
-        HostServiceTicket hostServiceTicket = m_hostServiceTickets.get(hostSystem);
-
-        if (!m_hostSystemCimUrls.containsKey(hostSystem)) {
-            String ipAddress = primaryIpAddress;
-
-            if (ipAddress == null) {
-                ipAddress = getPrimaryHostSystemIpAddress(hostSystem);
-            }
-
-
-            if (ipAddress == null) {
-                logger.warn("Cannot determine ip address for host system '{}'", hostSystem.getMOR().getVal());
-                return cimObjects;
-            }
-
-            m_hostSystemCimUrls.put(hostSystem, "https://" + ipAddress + ":5989");
-        }
-
-        String cimAgentAddress = m_hostSystemCimUrls.get(hostSystem);
-
-        String namespace = "root/cimv2";
-
-        UserPrincipal userPr = new UserPrincipal(hostServiceTicket.getSessionId());
-        PasswordCredential pwCred = new PasswordCredential(hostServiceTicket.getSessionId().toCharArray());
-
-        CIMNameSpace ns = new CIMNameSpace(cimAgentAddress, namespace);
-        CIMClient cimClient = new CIMClient(ns, userPr, pwCred);
-
-        // very important to query esx5 hosts
-        cimClient.useMPost(false);
-
-        CIMObjectPath rpCOP = new CIMObjectPath(cimClass);
-
-        Enumeration<?> rpEnm = cimClient.enumerateInstances(rpCOP);
-
-        while (rpEnm.hasMoreElements()) {
-            CIMObject rp = (CIMObject) rpEnm.nextElement();
-
-            cimObjects.add(rp);
-        }
-
-        return cimObjects;
-    }
-
-    /**
-     * Queries a host system for Cim data.
-     *
-     * @param hostSystem the host system to query
-     * @param cimClass   the class of Cim objects to retrieve
-     * @return the list of Cim objects
-     * @throws RemoteException
-     * @throws CIMException
-     */
-    public List<CIMObject> queryCimObjects(HostSystem hostSystem, String cimClass) throws ConnectException, RemoteException, CIMException {
-        return queryCimObjects(hostSystem, cimClass, null);
-    }
-
-    /**
-     * Searches for the primary ip address of a host system.
-     * <p>The idea is to resolve the HostSystem's name and use the resulting IP if the IP is listed on the available addresses list,
-     * otherwise, use the first ip listed on the available list.</p>
-     *
-     * @param hostSystem the host system to query
-     * @return the primary ip address
-     * @throws RemoteException
-     */
-    // TODO We should use the IP of the "Management Network" (i.e. the port that has enabled "Management Traffic" on the available vSwitches).
-    //      Resolving the name of the HostSystem as the FQDN is the most closest thing for that.
-    public String getPrimaryHostSystemIpAddress(HostSystem hostSystem) throws RemoteException {
-        TreeSet<String> addresses = getHostSystemIpAddresses(hostSystem);
-        String ipAddress = null;
-        try {
-            ipAddress = InetAddress.getByName(hostSystem.getName()).getHostAddress();
-        } catch (Exception e) {
-            logger.debug("Can't resolve the IP address from {}.", hostSystem.getName());
-        }
-        if (ipAddress == null) {
-            return addresses.first();
-        }
-        return addresses.contains(ipAddress) ? ipAddress : addresses.first();
-    }
-
-    /**
-     * Searches for all ip addresses of a host system
-     *
-     * @param hostSystem the host system to query
-     * @return the ip addresses of the host system, the first one is the primary
-     * @throws RemoteException
-     */
-    public TreeSet<String> getHostSystemIpAddresses(HostSystem hostSystem) throws RemoteException {
-        TreeSet<String> ipAddresses = new TreeSet<String>();
-
-        HostNetworkSystem hostNetworkSystem = hostSystem.getHostNetworkSystem();
-
-        if (hostNetworkSystem != null) {
-            HostNetworkInfo hostNetworkInfo = hostNetworkSystem.getNetworkInfo();
-            if (hostNetworkInfo != null) {
-                HostVirtualNic[] hostVirtualNics = hostNetworkInfo.getConsoleVnic();
-                if (hostVirtualNics != null) {
-                    for (HostVirtualNic hostVirtualNic : hostVirtualNics) {
-                        ipAddresses.add(hostVirtualNic.getSpec().getIp().getIpAddress());
-                    }
-                }
-                hostVirtualNics = hostNetworkInfo.getVnic();
-                if (hostVirtualNics != null) {
-                    for (HostVirtualNic hostVirtualNic : hostVirtualNics) {
-                        ipAddresses.add(hostVirtualNic.getSpec().getIp().getIpAddress());
-                    }
-                }
-            }
-        }
-        return ipAddresses;
-    }
-
-    /**
-     * Searches for all ip addresses of a virtual machine
-     *
-     * @param virtualMachine the virtual machine to query
-     * @return the ip addresses of the virtual machine, the first one is the primary
-     * @throws RemoteException
-     */
-    public TreeSet<String> getVirtualMachineIpAddresses(VirtualMachine virtualMachine) throws RemoteException {
-        TreeSet<String> ipAddresses = new TreeSet<String>();
-
-        // add the Ip address reported by VMware tools, this should be primary
-        if (virtualMachine.getGuest().getIpAddress() != null)
-            ipAddresses.add(virtualMachine.getGuest().getIpAddress());
-
-        // if possible, iterate over all virtual networks networks and add interface Ip addresses
-        if (virtualMachine.getGuest().getNet() != null) {
-            for (GuestNicInfo guestNicInfo : virtualMachine.getGuest().getNet()) {
-                if (guestNicInfo.getIpAddress() != null) {
-                    for (String ipAddress : guestNicInfo.getIpAddress()) {
-                        ipAddresses.add(ipAddress);
-                    }
-                }
-            }
-        }
-
-        return ipAddresses;
-    }
-
-    /**
-     * Searches for a managed entity by a given type.
-     *
-     * @param type the type string to search for
-     * @return the list of managed entities found
-     * @throws RemoteException
-     */
-    public ManagedEntity[] searchManagedEntities(String type) throws RemoteException {
-        return (new InventoryNavigator(m_serviceInstance.getRootFolder())).searchManagedEntities(type);
-    }
-
-    /**
-     * Return the major API version for this management server
-     *
-     * @return the major API version
-     */
-    public int getMajorApiVersion() {
-        if (m_serviceInstance != null) {
-            String apiVersion = m_serviceInstance.getAboutInfo().getApiVersion();
-
-            String[] arr = apiVersion.split("\\.");
-
-            if (arr.length > 1) {
-                int apiMajorVersion = Integer.valueOf(arr[0]);
-
-                if (apiMajorVersion < 4) {
-                    apiMajorVersion = 3;
-                }
-
-                return apiMajorVersion;
-            } else {
-                logger.error("Cannot parse vCenter API version '{}'", apiVersion);
-
-                return 0;
-            }
-        } else {
-            return 0;
-        }
-    }
-
-    /**
-     * Returns the value of a given cim object and property.
-     *
-     * @param cimObject    the Cim object
-     * @param propertyName the property's name
-     * @return the value
-     */
-    public String getPropertyOfCimObject(CIMObject cimObject, String propertyName) {
-        if (cimObject == null) {
-            return null;
-        } else {
-            CIMProperty cimProperty = cimObject.getProperty(propertyName);
-            if (cimProperty == null) {
-                return null;
-            } else {
-                CIMValue cimValue = cimProperty.getValue();
-                if (cimValue == null) {
-                    return null;
-                } else {
-                    Object object = cimValue.getValue();
-                    if (object == null) {
-                        return null;
-                    } else {
-                        return object.toString();
-                    }
-                }
-            }
-        }
     }
 }
 
